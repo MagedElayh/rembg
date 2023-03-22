6d5b5fc8d148addb450bdf14370f756d11c7700c
majed <magedelayh098@gmail.com>
2023-03-23 02:48:17 +0300

    optimaze image

--- main/views.py
+++ main/views.py
@@ -1,13 +1,12 @@
 
 import os
-os.environ['OPENBLAS_NUM_THREADS']='1'
 import base64
 from PIL import Image
 from io import BytesIO
 
 from rembg import remove
-from django.conf import settings
 from django.shortcuts import render
+os.environ['OPENBLAS_NUM_THREADS']='1'
 
 
 
@@ -26,14 +25,12 @@ def homePage(request):
                     "image_out" : image_out,
                 }
             return render(request,'base.html',context)
-        # image = numpy.array(image)
-
-        # image = Image.open('/path/to/your/image.jpg')
+        
         image = Image.open(image)
 
 
-        if image.mode == 'RGBA':
-            image = image.convert('RGB')
+        # if image.mode == 'RGBA':
+        #     image = image.convert('RGB')
 
         """ remove bg here """
         image_out = remove(image)
@@ -49,10 +46,13 @@ def homePage(request):
 
         # Convert the image to a base64-encoded string
         buffered = BytesIO()
-        image.save(buffered, format="JPEG")
+        try:
+            image.save(buffered, format=image.format)
+        except ValueError:
+            image.save(buffered, format="PNG")
         image = base64.b64encode(buffered.getvalue()).decode()
 
-        # print(image,"###############################")
+
     else:
         image = None
     form =None
